3b43842903f9912c9e13e23293d383b36dbe9896
xjoelee@gmail.com <ggj2nnyg00>
2019-12-20 17:18:20 +0900

    m

--- src/com/underslow/Main.java
+++ src/com/underslow/Main.java
@@ -3,6 +3,6 @@ package com.underslow;
 public class Main {
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello java, aT Main.underslow.com");
+        System.out.println("Hello java, aT Main.underslow.com...");
     }
 }
